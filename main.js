a7ce97ec04bc3e4238fecc2e7c9fd592a0d7763e
Jea-nyl <napalingajeanyl1@gmail.com>
2025-12-17 19:39:18 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 import { auth } from "./firebase.js";
-import { signInWithEmailAndPassword } from
-  "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const status = document.getElementById("status");
 
-document.getElementById("loginBtn").addEventListener("click", async () => {
+document.getElementById("registerBtn").addEventListener("click", async () => {
   try {
-    await signInWithEmailAndPassword(
+    await createUserWithEmailAndPassword(
       auth,
       email.value,
       password.value
     );
-    status.textContent = "Login successful ✅";
+    status.textContent = "Account created ✅";
   } catch (err) {
     status.textContent = err.message;
   }
